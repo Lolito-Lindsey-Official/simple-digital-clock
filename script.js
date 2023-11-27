ed0870ac658ca2a0e2c6f5bd5401a2bcc3c74b84
lolito-lindsey-official <lolitolindsey@gmail.com>
2023-11-27 11:59:15 -0600

    Added event listener to 'add new task' button
    Adds a new node (listItem) to the todo-item (todo-items).

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ function cycleClock() {
 
 // 1000ms = 1 second
 setInterval(cycleClock, 1000);
+
+// When the user clicks on "Add New Task", get the button id to add the task
+const addTask = document.getElementById('add-todo');
+
+addTask.addEventListener('click', function() {
+        const inputField = document.getElementById('todo-input');
+        const makeNewTask =inputField.value;
+
+        if (makeNewTask.trim() !== ' ') {
+                const listItem = document.createElement('li');
+                // Connects the created element with the input field, adds new task to the list
+                listItem.innerText = makeNewTask;
+                /* 
+                        adds a new task = adds a node to the end of the list of children of a specified parent node
+
+                        node = listItem;
+                        parent node = todo-items;
+
+                        Reference: https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+                */
+                document.getElementById('todo-items').appendChild(listItem);
+        }
+})
